2444865efb43714d79ed78bc9acfe2fc20bf40d5
Ericson Moreira <moreira.ericson@gmail.com>
2022-09-21 09:20:46 -0300

    Add documentação em Util.java

--- src/main/java/com/esp/govbrsignatureintegration/utils/Util.java
+++ src/main/java/com/esp/govbrsignatureintegration/utils/Util.java
@@ -2,7 +2,6 @@ package com.esp.govbrsignatureintegration.utils;
 
 import com.itextpdf.signatures.BouncyCastleDigest;
 import com.itextpdf.signatures.DigestAlgorithms;
-import org.apache.commons.codec.digest.MessageDigestAlgorithms;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +11,12 @@ import java.util.Base64;
 public class Util {
 
     /**
-     * Retorna o base64 de um hash SHA-256
+     * Retorna o base64 de um hash SHA-256.
+     *
+     * @param data {@link InputStream} com a informação para gerar o hash SHA-256.
+     * @return {@link String} do hash SHA-256.
+     * @throws GeneralSecurityException
+     * @throws IOException
      */
     public static String generateHashSHA256(InputStream data) throws GeneralSecurityException, IOException {
         BouncyCastleDigest digest = new BouncyCastleDigest();
